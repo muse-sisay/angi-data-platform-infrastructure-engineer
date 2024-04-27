285d30a4f6f1e37400659ffc91084f022d863e67
b_sage <muse@gc.home.arpa>
2024-04-27 03:39:03 -0600

    feat: use points endpoint to reterive forecast office and gridpoint
    - log operation being performed

--- app/main.py
+++ app/main.py
@@ -4,13 +4,35 @@ from configparser import ConfigParser
 import requests
 import json
 import sys
+import logging
+from pprint import pprint 
 
-def writeJsonToFile( outputPath, filename, data) :
+def loadConfig( configFileName):
+    """
+    reads config file and returns outputPath, latitude and longitude
+    """
+    #TODO check if config file exists
+    logging.info("loading config file")
+    config  = ConfigParser()
+    config.read(configFileName)
+    logging.info(f"loaded config file \"{configFileName}\".")
+
+    outputPath = config["OUTPUT"]["path"]
+    longitude = config["LOCATION"]["longitude"]
+    latitude = config["LOCATION"]["latitude"]
+
+    logging.info(f"output path {outputPath} , latitude {latitude}, longitude {longitude}")
+    return outputPath , latitude, longitude
+
+
+def writeJsonToFile( outputPath, filename, data):
+    logging.info(f"writing forecast data to {outputPath}/{filename}.json")
     with open( f"{outputPath}/{filename}.json" , "w") as file :
         json.dump(data, file )
 
 def getJsonResponse( url ):
-    try :
+    try : 
+        logging.info(f"fetching forecast from {url}")
         response = requests.get(url)
         data = json.loads(response.text)
         # TODO check response.status
@@ -18,24 +40,28 @@ def getJsonResponse( url ):
     except Error: 
         sys.exit(1)
 
+def reteriveforecastHourlyURL(latitude, longitude):
+    try:
+        url = f"https://api.weather.gov/points/{latitude},{longitude}"
+        response = requests.get(url)
+        data = json.loads(response.text)
+        forecastHourlyURL = data['properties']['forecastHourly']
+        logging.info(f"grid point url {forecastHourlyURL}")
+        return forecastHourlyURL
+        
+    except Error:
+        pass
 
-
-#TODO write log to STDOUT
 
 if __name__ ==  "__main__" :
-    #TODO check if config file exists
-    config  = ConfigParser()
-    config.read('config.ini')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
-    outputPath = config["OUTPUT"]["path"]
-    forecastOfficeID = config["LOCATION"]["forecast_office_id"]
-    longitude = config["LOCATION"]["x_coordinate"]
-    latitude = config["LOCATION"]["y_coordinate"]
+    outputPath, latitude, longitude = loadConfig("config.ini")
 
-    reqUrl = f"https://api.weather.gov/gridpoints/{forecastOfficeID}/{latitude},{longitude}/forecast/hourly"
-
-    data = getJsonResponse(reqUrl)
+    hourlyForecastURL = reteriveforecastHourlyURL(latitude, longitude)
+    
+    hourlyForecast = getJsonResponse(hourlyForecastURL)
 
     now = datetime.now(timezone.utc)
     dateNowString  = now.strftime("%Y-%m-%d_%H%M%S")
-    writeJsonToFile(outputPath, dateNowString, data)
+    writeJsonToFile(outputPath, dateNowString, hourlyForecast)
